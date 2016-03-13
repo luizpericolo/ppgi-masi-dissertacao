0fbce5cdb4b80698d0cec7a2761c93f66133167c
Luiz Pericolo <luizpericolo@gmail.com>
2016-03-13 15:11:34 -0300

    Add timing decorator for performance measuring

---
+++ src/timer.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+import time
+
+
+def timeit(fn):
+    def timed(*args, **kwargs):
+        ts = time.time()
+        result = fn(*args, **kwargs)
+        te = time.time()
+
+        print "{}({}, {}) - {:2.4f} seconds".format(fn.__name__, args, kwargs, te - ts)
+        return result
+
+    return timed
