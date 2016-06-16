9800b91e18aa71b0b2ba54509417b6857ff8ef73
Luiz Pericolo <luizpericolo@gmail.com>
2016-06-15 21:29:51 -0300

    add corridor class to map

--- src/map.py
+++ src/map.py
@@ -51,25 +51,26 @@ class Room:
 class Map:
 	def __init__(self, max_width=50, max_height=50):
 		self.rooms = []
+		self.corridors = []
+		self.room_map = {}
 		self.max_width = max_width
 		self.max_height = max_height
 
-	def add_room(self, x, y, width, height):
+	def add_room(self, room_id, x, y, width, height):
 		""" Creates and adds a room to the map. """
-
-		next_room_index = len(self.rooms)
 
 		while True:
 			can_place = True
 			x = random.randrange(0, 100)
 			y = random.randrange(0, 100)
-			new_room = Room(next_room_index, x, y, width, height)
+			new_room = Room(room_id, x, y, width, height)
 			for room in self.rooms:
 				if new_room.intersect(room):
 					can_place = False
 					break
 			if can_place:
 				self.rooms.append(new_room)
+				self.room_map[room_id] = new_room
 				break
 
 	def print_rooms(self):
@@ -78,13 +79,29 @@ class Map:
 		for room in self.rooms:
 			room.print_room()
 
+	def print_corridors(self):
+		""" Prints a map's corridor list. """
+		for corridor in self.corridors:
+			corridor.print_corridor()
+
+	def add_corridor(self, origin, destination):
+		origin_room = self.room_map.get(origin)
+		destination_room = self.room_map.get(destination)
+		new_corridor = Corridor(origin_room, destination_room)
+
+		self.corridors.append(new_corridor)
+
 	def import_from_graph(self, graph):
 		for node in graph.nodes:
 			w, h = self.get_random_room_dimensions()
 
 			# XXX: Improve this x=None, y=None.
 			# Check add_room.
-			self.add_room(None, None, w, h)
+			self.add_room(node, None, None, w, h)
+
+		for edge in graph.edges:
+			origin, destination = edge
+			self.add_corridor(origin, destination)
 
 	def get_random_room_dimensions(self):
 		""" Returns a tuple (w, h) with random values. """
@@ -103,4 +120,21 @@ class Map:
 			coords = list(room._switch_coordinates())
 			draw.rectangle(coords, fill, outline)
 
+		corridor_fill = 'yellow'
+		for corridor in self.corridors:
+			draw.line(corridor.coords, fill)
+
 		im.save('test.bmp')
+
+
+class Corridor:
+	def __init__(self, origin, destination):
+		self._origin = origin
+		self._destination = destination
+
+	@property
+	def coords(self):
+		return tuple(self._origin.center) + tuple(self._destination.center)
+
+	def print_corridor(self):
+		print(u"From room {origin} to room {destination}".format(origin=self._origin.id, destination=self._destination.id))
